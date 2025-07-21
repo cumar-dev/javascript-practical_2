3173c6234aa1d98aba478103439585d82c865eff
cumar-dev <cumarcabdicumar2022@gmail.com>
2025-07-21 19:16:54 +0400

    add prevent default update

--- Prevent Default Actions/main.js
+++ Prevent Default Actions/main.js
@@ -1,9 +1,11 @@
 const Form = document.querySelector('#myForm');
 const submitData = document.querySelector('#submitData');
-
+const name = document.querySelector('#name');
 Form.addEventListener('submit', function(event) {
     event.preventDefault();
-    submitData.textContent = 'Enter your name';
-    // console.log("enter your name");
+    const nameInput = document.createElement('li');
+    nameInput.style.listStyle = 'none';
+    nameInput.textContent = `${name.value}`;
+    submitData.appendChild(nameInput);
 })
 
